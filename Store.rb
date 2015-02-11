b3a1da4e43e595df20d203ca683e83ba4b424ec3
Jacob Adelgren <jadelgre@mymail.mines.edu>
2015-02-11 16:00:20 -0700

    created base rules, empty classes

---
+++ Store.rb
@@ -0,0 +1,24 @@
+class Store
+
+	def initialize
+		@products = []
+	end
+	
+	def add_product
+		
+	end
+	
+	def run_store
+	
+	end
+	
+end
+
+class Product
+	attr_reader: actions
+	
+	def intialize(text)
+		@product_name = text
+	end
+	
+end
---
+++ rules.rb
@@ -0,0 +1,33 @@
+def product(text)
+  #puts "Read in product#{text}"
+end
+
+def email(text)
+
+end
+
+def notify(text)
+
+end
+
+def packing_slip(text)
+
+end
+
+def pay(text)
+
+end
+
+def include(text)
+
+end
+
+def activate
+
+end
+
+def includes_free(text)
+
+end
+
+load 'businessRules.txt'
